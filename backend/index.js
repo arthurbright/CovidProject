d84dd2a753aab69a5addc1469dbebcbdca4be4f1
arthurbright <55501753+arthurbright@users.noreply.github.com>
2020-08-11 23:04:35 -0400

    a

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const fs = require('fs').promises;
+
+
+const app = express();
+app.use(express.json());
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Listening on port 3000...");
+});
